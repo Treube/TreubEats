a7559d4aff36122922d7369694f70b8cc7aceb55
SutherlandB <41080577+SutherlandB@users.noreply.github.com>
2024-02-27 16:29:28 -0500

    Database

--- app/app/components/ProductCard.tsx
+++ app/app/components/ProductCard.tsx
@@ -8,3 +8,5 @@ const ProductCard = () => {
         </div>
     )
 }
+
+export default ProductCard
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>TreubeEats</h1>
+      <ProductCard />
       </main>
   );
 }
