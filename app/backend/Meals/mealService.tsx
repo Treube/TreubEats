df3756c186de4a8c1ef1b6bc48c62e3c740f62aa
Jeda Coward <jeda@mjcowards.com>
2024-04-09 18:16:52 -0400

    Adding to meal service

--- app/backend/Meals/mealService.tsx
+++ app/backend/Meals/mealService.tsx
@@ -1,23 +1,47 @@
 import prisma from '../Prisma'
 import * as userService from './mealService' ;
 
-export async function createMeal( name: string, price: number): Promise<any>{
+export async function createMeal( name: string, description: string, price: number ): Promise<any>{
     //have some requirements about user name and password 
     /**
      * ensure password is within some criteria
      */
     //Create the user using prisma client
-    console.log("I am about to print new neal");
     const newMeal= await prisma.meal.create({
         data: {
-            name:name,
-            description: "this is an amazing meal",
+            name: name,
+            description: description,
             price: price,
-            },
-        }
-    );
-    console.log("I am about to print new neal");
-    console.log(newMeal);
+        },
+    });
     return newMeal;
 };
 
+export async function editMeal( mealId: number, name: string, description: string, price: number ): Promise<any>{
+    //have some requirements about user name and password 
+    /**
+     * ensure password is within some criteria
+     */
+    //Create the user using prisma client
+    const updateMeal= await prisma.meal.update({
+        where: {
+            id: mealId
+        },
+        data: {
+            name: name,
+            description: description,
+            price: price,
+        },
+    });
+    return updateMeal;
+};
+
+// this will not be shown on the main website, only admin logins can see this
+export async function deleteMeal( userId: number, mealId: number): Promise<any>{
+    const deletedMeal = await prisma.meal.delete({
+      where: {
+        id: mealId
+      },
+    });
+    return deletedMeal;
+}
