b238065e6a9f091a75f0aeb114dd2848e7265357
Jeda Coward <jeda@mjcowards.com>
2024-04-24 22:12:54 -0400

    Update mealController.tsx

--- app/backend/Meals/mealController.tsx
+++ app/backend/Meals/mealController.tsx
@@ -41,16 +41,14 @@ export const deleteMeal= async(req: NextRequest , res: NextResponse) => {
   // ensure that body has a name and description and price
   // retrieve name and items from req
   const request_data = await req.formData();
-  const name = request_data.get("name") as string;
-  const description = request_data.get("description") as string;
-  const price = parseFloat(request_data.get("price") as string);
-  const id = parseInt(request_data.get("id") as string);
+  const userId = parseInt(request_data.get("userId") as string);
+  const mealId = parseInt(request_data.get("mealId") as string);
 
-  if (!id && (!name || !price || !description )) {
-    console.log(id,name,description,price);
-    return NextResponse.error();
+  if (!userId && !mealId) {
+    console.log(userId, mealId);
+    return NextResponse.error();  
   }
-  const deletedMeal = await MealService.deleteMeal(reviewId, userId);
+  const deletedMeal = await MealService.deleteMeal(mealId, userId);
   return deletedMeal;
   // respond with the created meal
 };
